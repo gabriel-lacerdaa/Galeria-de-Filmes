d3e418522644b1bfc88e0136731bb0152dfdc8e1
Gabriel Lacerda <gabriel.lacerda1@outlook.com>
2023-03-28 19:50:31 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+import requests
+
+app = Flask(__name__, static_folder='static')
+
+api_key = 'c99099d0'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/busca', methods=['POST', 'GET'])
+def busca():
+    nome_do_filme = request.form['filme']
+    return redirect(f'/filmes/{nome_do_filme}')
+
+
+@app.route('/filmes/<nome_do_filme>')
+def filme(nome_do_filme):
+    imagens_html = ''
+    url = f'https://www.omdbapi.com/?apikey={api_key}&s={nome_do_filme}&type=movie'
+    response = requests.get(url)
+    dicionario = response.json()
+    if 'Error' in dicionario.keys():
+        return '<h1>Filme não encontrado</h1>'
+    for filme in dicionario['Search'][0:10]:
+        imagens_html += f'<img src="{filme["Poster"]}"/></img>'
+    return imagens_html
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
